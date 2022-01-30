41bc707c0b5669c5047b399c0f3970776bf0d05c
Leonardo Gurgel <lgurgel@id.uff.br>
2022-01-30 19:38:43 -0300

    add points generator

---
+++ points.py
@@ -0,0 +1,16 @@
+from random import randrange, random
+
+
+def generate_coordinate(max: int) -> float:
+    return randrange(0, max - 1) + random()
+
+
+for size in range(21, 31):
+    list = []
+    for i in range(2**size):
+        list.append([generate_coordinate(500), generate_coordinate(500)])
+    list.sort(key=lambda a: a[0])
+    with open(f'points/{size}.txt', 'w') as f:
+        f.write(f'{2**size}\n')
+        for item in list:
+            f.write(f'{item[0]}\n{item[1]}\n')
